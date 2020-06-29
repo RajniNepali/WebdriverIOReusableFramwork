4b288271cc604556d9b04409947706746b1cf8da
RajniNepali <rajninepali0694@gmail.com>
2020-06-29 16:29:37 +0530

    Commit

---
+++ ConfKeys/DB_config.js
@@ -0,0 +1,9 @@
+module.exports.databaseOptions = {
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Test123',
+    database : 'test',
+    demo     : 'http://www.webdriveruniversity.com/',
+   //Updated
+  }
+

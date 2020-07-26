df7e0ecf8ec22aa8322f0f8fa486237bec4d827e
SayaliBankar-git <62230825+SayaliBankar-git@users.noreply.github.com>
2020-07-26 06:53:42 -0700

    Added function readDatabase

--- MySql/readDatabase.js
+++ MySql/readDatabase.js
@@ -3,17 +3,18 @@ var mysql = require('mysql');
 var connection = mysql.createConnection(config.databaseOptions);
 var result;
 
-
-    connection.connect((err) => {
+connection.connect((err) => {
         if (err) throw err;
         console.log('Conneted!');
       });
-
-      module.exports.ReadDatabase = connection.query('SELECT * FROM contactus', (err,rows) => {
+//module.exports.ReadDatabase = 
+  function readDatabase(){
+    connection.query('SELECT * FROM contactus', (err,rows) => {
         if(err) throw err;
-          console.log('Data received from Db:');
+          //console.log('Data received from Db:' +rows);       
           console.log(rows);
           result = rows;
       });
+   }
        
  
